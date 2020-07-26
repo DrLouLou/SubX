8c6d1e3867af7c7726448af0220b6fe6a8a1985e
Louis Choo-Choy <lchoochoy@gmail.com>
2020-07-26 13:54:44 +0100

    added django-heroku settings

--- SubX/settings.py
+++ SubX/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -130,3 +131,5 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
 # List of directories that django will look in for serving static assets.
+
+django_heroku.settings(locals())
